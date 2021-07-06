9a3b446858b2c2417d5750c1d8d58e02f89c0297
ROGi0228 <84007090+ROGi0228@users.noreply.github.com>
2021-07-06 17:28:07 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight: true,
+        slidesToShow: 2,
+        speed: 1000,
+        easing: 'linear',
+        autoplay: true,
+        autoplaySpeed: 10000,
+        centerMode: true,
+        variableWidth: false,
+        responsive: [{
+            breakpoint: 768,
+            settings:{
+                slidesToShow: 1,
+                variableWidth: true,
+            }
+        }]
+        
+    });
+
+
+
+
+
+    //E-mail Ajax Send
+	$("#form__send").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
+})
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+
+for (let anchor of anchors){
+    anchor.addEventListener("click", function(event){
+        event.preventDefault();
+        const blockID = anchor.getAttribute('href')
+        document.querySelector('' + blockID).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
+
+
+
+
+
+  $(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger, .navigation__links, .main-information, .navigation__logo1, .navigation__logo').toggleClass('active');
+    })
+  })
